502993800bcde72b6751ced90a0c41368d3b4437
vargas200YT <140112236+vargas200@users.noreply.github.com>
2023-11-02 17:42:01 -0600

    Add files via upload

---
+++ proyecto 84/ADV 1-4 C84 Plantilla/main_84.js
@@ -0,0 +1,13 @@
+pizza_list_array = [];
+
+function getmenu()
+	{    
+        // Da un nombre apropiado de identificación como nombre_pizza.
+        var item = document.getElementById("nombre_pizza").value; 
+       
+        //Agrega la función push() para llevar los elementos al array.
+        pizza_list_array.push(item);
+        
+        var remove_commas = pizza_list_array.join("<br># ");
+        document.getElementById("mostrar_menú").innerHTML =  "# " + remove_commas;
+	}
